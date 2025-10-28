f87e64ce665ad8a4dc56e57935776cce4e448ab9
iron-LAN <62120423+davidrustingha@users.noreply.github.com>
2025-10-28 15:01:22 +0100

    Update sensor.py

--- components/opentherm/sensor.py
+++ components/opentherm/sensor.py
@@ -1,29 +1,31 @@
 from typing import Any, Dict
-
 import esphome.config_validation as cv
 from esphome.components import sensor
 
 from . import const, schema, validate, generate
 
-DEPENDENCIES = [ const.OPENTHERM ]
+DEPENDENCIES = [const.OPENTHERM]
 COMPONENT_TYPE = const.SENSOR
 
 def get_entity_validation_schema(entity: schema.SensorSchema) -> cv.Schema:
+    """Return the sensor schema with defaults for optional fields."""
     return sensor.sensor_schema(
-        unit_of_measurement = entity.get("unit_of_measurement", ""),
-        accuracy_decimals = entity["accuracy_decimals"],
-        device_class=entity.get("device_class", ""),
-        icon = entity.get("icon", ""),
-        state_class = entity["state_class"]
+        unit_of_measurement=entity.get("unit_of_measurement", ""),
+        accuracy_decimals=entity.get("accuracy_decimals", 1),
+        device_class=entity.get("device_class", "none"),
+        icon=entity.get("icon", ""),
+        state_class=entity.get("state_class", "measurement")
     )
 
-CONFIG_SCHEMA = validate.create_component_schema(schema.SENSORS, get_entity_validation_schema)
+CONFIG_SCHEMA = validate.create_component_schema(
+    schema.SENSORS, get_entity_validation_schema
+)
 
 async def to_code(config: Dict[str, Any]) -> None:
     await generate.component_to_code(
-        COMPONENT_TYPE, 
+        COMPONENT_TYPE,
         schema.SENSORS,
-        sensor.Sensor, 
-        generate.create_only_conf(sensor.new_sensor), 
+        sensor.Sensor,
+        generate.create_only_conf(sensor.new_sensor),
         config
     )
