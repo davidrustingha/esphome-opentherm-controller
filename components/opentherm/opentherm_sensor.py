88b71fd44bc18c5f3c3d99f28feac1eb3df9c73e
iron-LAN <62120423+davidrustingha@users.noreply.github.com>
2025-10-28 15:25:21 +0100

    Update and rename sensor.py to opentherm_sensor.py

---
+++ components/opentherm/opentherm_sensor.py
@@ -0,0 +1,29 @@
+from typing import Any, Dict
+
+import esphome.config_validation as cv
+from esphome.components import sensor as esph_sensor  # renamed import
+
+from . import const, schema, validate, generate
+
+DEPENDENCIES = [const.OPENTHERM]
+COMPONENT_TYPE = const.SENSOR
+
+def get_entity_validation_schema(entity: schema.SensorSchema) -> cv.Schema:
+    return esph_sensor.sensor_schema(
+        unit_of_measurement=entity.get("unit_of_measurement", ""),
+        accuracy_decimals=entity.get("accuracy_decimals", 0),
+        device_class=entity.get("device_class", ""),
+        icon=entity.get("icon", ""),
+        state_class=entity.get("state_class", "")
+    )
+
+CONFIG_SCHEMA = validate.create_component_schema(schema.SENSORS, get_entity_validation_schema)
+
+async def to_code(config: Dict[str, Any]) -> None:
+    await generate.component_to_code(
+        COMPONENT_TYPE,
+        schema.SENSORS,
+        esph_sensor.Sensor,
+        generate.create_only_conf(esph_sensor.new_sensor),
+        config
+    )
