5427d1aa7614d5da5d45e76edfae359e6a53ad06
iron-LAN <62120423+davidrustingha@users.noreply.github.com>
2025-10-28 14:53:43 +0100

    Update sensor.py

--- components/opentherm/sensor.py
+++ components/opentherm/sensor.py
@@ -10,10 +10,10 @@ COMPONENT_TYPE = const.SENSOR
 
 def get_entity_validation_schema(entity: schema.SensorSchema) -> cv.Schema:
     return sensor.sensor_schema(
-        unit_of_measurement = entity.get("unit_of_measurement", None),
+        unit_of_measurement = entity.get("unit_of_measurement", ""),
         accuracy_decimals = entity["accuracy_decimals"],
-        device_class=entity.get("device_class", None),
-        icon = entity.get("icon", None),
+        device_class=entity.get("device_class", ""),
+        icon = entity.get("icon", ""),
         state_class = entity["state_class"]
     )
 
