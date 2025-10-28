6bd54198128915034f6bf7b0ebe7cee18bfb392a
iron-LAN <62120423+davidrustingha@users.noreply.github.com>
2025-10-28 14:15:03 +0100

    Update binary_sensor.py

--- components/opentherm/binary_sensor.py
+++ components/opentherm/binary_sensor.py
@@ -5,13 +5,13 @@ from esphome.components import binary_sensor
 
 from . import const, schema, validate, generate
 
-DEPENDENCIES = [ const.OPENTHERM ]
+DEPENDENCIES = [const.OPENTHERM]
 COMPONENT_TYPE = const.BINARY_SENSOR
 
 def get_entity_validation_schema(entity: schema.BinarySensorSchema) -> cv.Schema:
     return binary_sensor.binary_sensor_schema(
-        device_class = entity["device_class"] if "device_class" in entity else binary_sensor._UNDEF,
-        icon = entity["icon"] if "icon" in entity else binary_sensor._UNDEF
+        device_class=entity.get("device_class", None),
+        icon=entity.get("icon", None)
     )
 
 CONFIG_SCHEMA = validate.create_component_schema(schema.BINARY_SENSORS, get_entity_validation_schema)
@@ -20,7 +20,7 @@ async def to_code(config: Dict[str, Any]) -> None:
     await generate.component_to_code(
         COMPONENT_TYPE,
         schema.BINARY_SENSORS,
-        binary_sensor.BinarySensor, 
-        generate.create_only_conf(binary_sensor.new_binary_sensor), 
+        binary_sensor.BinarySensor,
+        generate.create_only_conf(binary_sensor.new_binary_sensor),
         config
     )
