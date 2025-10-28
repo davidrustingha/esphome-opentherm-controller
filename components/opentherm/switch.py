fa19d5409a5b3180540e9efe2b126d3788de31db
iron-LAN <62120423+davidrustingha@users.noreply.github.com>
2025-10-28 21:31:10 +0100

    Update switch.py

--- components/opentherm/switch.py
+++ components/opentherm/switch.py
@@ -7,10 +7,12 @@ from esphome.const import CONF_ID
 
 from . import const, schema, validate, generate
 
-DEPENDENCIES = [ const.OPENTHERM ]
+DEPENDENCIES = [const.OPENTHERM]
 COMPONENT_TYPE = const.SWITCH
 
-OpenthermSwitch = generate.opentherm_ns.class_("OpenthermSwitch", switch.Switch, cg.Component)
+OpenthermSwitch = generate.opentherm_ns.class_(
+    "OpenthermSwitch", switch.Switch, cg.Component
+)
 
 CONF_MODE = "mode"
 
@@ -22,25 +24,34 @@ async def new_openthermswitch(config: Dict[str, Any]) -> cg.Pvariable:
     return var
 
 def get_entity_validation_schema(entity: schema.SwitchSchema) -> cv.Schema:
-    return switch.SWITCH_SCHEMA.extend({
+    return switch.switch_schema().extend({
         cv.GenerateID(): cv.declare_id(OpenthermSwitch),
-        cv.Optional(CONF_MODE, entity["default_mode"]): 
-            cv.enum({
-                "restore_default_on": cg.RawExpression("opentherm::OpenthermSwitchMode::OPENTHERM_SWITCH_RESTORE_DEFAULT_ON"), 
-                "restore_default_off": cg.RawExpression("opentherm::OpenthermSwitchMode::OPENTHERM_SWITCH_RESTORE_DEFAULT_OFF"),
-                "start_on": cg.RawExpression("opentherm::OpenthermSwitchMode::OPENTHERM_SWITCH_START_ON"),
-                "start_off": cg.RawExpression("opentherm::OpenthermSwitchMode::OPENTHERM_SWITCH_START_OFF")
-            })
+        cv.Optional(CONF_MODE, entity["default_mode"]): cv.enum({
+            "restore_default_on": cg.RawExpression(
+                "opentherm::OpenthermSwitchMode::OPENTHERM_SWITCH_RESTORE_DEFAULT_ON"
+            ),
+            "restore_default_off": cg.RawExpression(
+                "opentherm::OpenthermSwitchMode::OPENTHERM_SWITCH_RESTORE_DEFAULT_OFF"
+            ),
+            "start_on": cg.RawExpression(
+                "opentherm::OpenthermSwitchMode::OPENTHERM_SWITCH_START_ON"
+            ),
+            "start_off": cg.RawExpression(
+                "opentherm::OpenthermSwitchMode::OPENTHERM_SWITCH_START_OFF"
+            )
+        })
     }).extend(cv.COMPONENT_SCHEMA)
 
-CONFIG_SCHEMA = validate.create_component_schema(schema.SWITCHES, get_entity_validation_schema)
+CONFIG_SCHEMA = validate.create_component_schema(
+    schema.SWITCHES, get_entity_validation_schema
+)
 
 async def to_code(config: Dict[str, Any]) -> None:
     keys = await generate.component_to_code(
-        COMPONENT_TYPE, 
+        COMPONENT_TYPE,
         schema.SWITCHES,
-        OpenthermSwitch, 
-        generate.create_only_conf(new_openthermswitch), 
+        OpenthermSwitch,
+        generate.create_only_conf(new_openthermswitch),
         config
     )
     generate.define_readers(COMPONENT_TYPE, keys)
